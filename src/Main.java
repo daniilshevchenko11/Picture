31217f3a2eccdeda181ca676dd546b41ca5a8176
daniilshevchenko11 <mrdaniilshevchenko@gmail.com>
2022-08-10 20:06:22 +0300

    public class Main {     public static void main(String[] args) {         System.out.println("Frog");         System.out.println("  "+(char)64+(char)46+(char)46+(char)64);         System.out.println(" "+(char)40+(char)45+(char)45+(char)45+(char)45+(char)41);         System.out.println(""+(char)40+" "+(char)62+(char)95+(char)95+(char)60+" "+(char)41);         System.out.println(""+(char)94+(char)94+" "+(char)126+(char)126+" "+(char)94+(char)94);
            System.out.println("Cat");
            System.out.println(" "+(char)47+(char)92+(char)95+(char)95+(char)47+(char)92)   ;
            System.out.println(" "+(char)40+(char)61+(char)94+(char)46+(char)94+(char)61+(char)41);
            System.out.println(" "+(char)40+(char)34+(char)41+" "+(char)40+(char)34+(char)41+(char)95+(char)47);

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Frog");
+        System.out.println("  "+(char)64+(char)46+(char)46+(char)64);
+        System.out.println(" "+(char)40+(char)45+(char)45+(char)45+(char)45+(char)41);
+        System.out.println(""+(char)40+" "+(char)62+(char)95+(char)95+(char)60+" "+(char)41);
+        System.out.println(""+(char)94+(char)94+" "+(char)126+(char)126+" "+(char)94+(char)94);
+
+        System.out.println("Cat");
+        System.out.println(" "+(char)47+(char)92+(char)95+(char)95+(char)47+(char)92)   ;
+        System.out.println(" "+(char)40+(char)61+(char)94+(char)46+(char)94+(char)61+(char)41);
+        System.out.println(" "+(char)40+(char)34+(char)41+" "+(char)40+(char)34+(char)41+(char)95+(char)47);
+
+
+
+
+    }
+}
